af0a21a7d0cfd9bcd50d37ef3633f928f07a2f74
Adrianus Kleemans <a.kleemans@gmail.com>
2020-10-23 16:35:55 +0200

    Add Diffie-Hellman page

--- src/diffie-hellman/diffie-hellman.js
+++ src/diffie-hellman/diffie-hellman.js
@@ -3,33 +3,19 @@ var Person;
     Person["ALICE"] = "Alice";
     Person["BOB"] = "Bob";
 })(Person || (Person = {}));
-var NumberSize;
-(function (NumberSize) {
-    NumberSize["SMALL"] = "Small";
-    NumberSize["BIG"] = "Big";
-})(NumberSize || (NumberSize = {}));
 var DiffieHellmanCtrl = /** @class */ (function () {
     function DiffieHellmanCtrl() {
         this.me = Person.ALICE;
-        this.numberSize = NumberSize.SMALL;
     }
     DiffieHellmanCtrl.prototype.$onInit = function () {
-        console.log('Starting up!');
+        console.log('Starting up! I am:', this.me);
     };
     // Function for Alice
     DiffieHellmanCtrl.prototype.prepareAlice = function () {
         // Initialize primes based on number size
-        if (this.numberSize === NumberSize.SMALL) {
-            this._p = 23n;
-            this._g = 5n;
-            this._a = 4n;
-        }
-        else {
-            // Group with id 14 from https://tools.ietf.org/html/rfc3526
-            this._p = 32317006071311007300338913926423828248817941241140239112842009751400741706634354222619689417363569347117901737909704191754605873209195028853758986185622153212175412514901774520270235796078236248884246189477587641105928646099411723245426622522193230540919037680524235519125679715870117001058055877651038861847280257976054903569732561526167081339361799541336476559160368317896729073178384589680639671900977202194168647225871031411336429319536193471636533209717077448227988588565369208645296636077250268955505928362751121174096972998068410554359584866583291642136218231078990999448652468262416972035911852507045361090559n;
-            this._g = 2n;
-            this._a = this.getSecretNumber();
-        }
+        this._p = 179;
+        this._g = 2;
+        this._a = this.getSecretNumber();
         this.A = this.modExp(this._g, this._a, this._p);
     };
     // Function for Bob
@@ -39,36 +25,18 @@ var DiffieHellmanCtrl = /** @class */ (function () {
     };
     // Gets a secret number, for example a or b.
     DiffieHellmanCtrl.prototype.getSecretNumber = function () {
-        // https://tools.ietf.org/html/rfc5054#section-3.1: "The private values a and b SHOULD be at least 256-bit
-        // random numbers, to give approximately 128 bits of security against certain methods of calculating discrete
-        // logarithms.
-        // TODO
-        if (this.numberSize === NumberSize.SMALL) {
-            return BigInt(Math.floor(Math.random() * 20));
-        }
-        else {
-            return this.getBigRandomNumber();
-        }
-    };
-    DiffieHellmanCtrl.prototype.getBigRandomNumber = function () {
-        var array = new Uint32Array(8);
-        window.crypto.getRandomValues(array);
-        var s = '0x';
-        for (var i = 0; i < array.length; i++) {
-            s += array[i].toString(16);
-        }
-        return BigInt(s);
+        return Math.floor(Math.random() * 1000);
     };
     // Fast modular exponentiation for a ^ b mod n
     // from https://gist.github.com/krzkaczor/0bdba0ee9555659ae5fe
     DiffieHellmanCtrl.prototype.modExp = function (a, b, n) {
         a = a % n;
-        var result = 1n;
+        var result = 1;
         var x = a;
         while (b > 0) {
-            var leastSignificantBit = b % 2n;
-            b = b / 2n;
-            if (leastSignificantBit == 1n) {
+            var leastSignificantBit = b % 2;
+            b = b / 2;
+            if (leastSignificantBit === 1) {
                 result = result * x;
                 result = result % n;
             }
@@ -77,7 +45,39 @@ var DiffieHellmanCtrl = /** @class */ (function () {
         }
         return result;
     };
-    ;
+    DiffieHellmanCtrl.prototype.calculateKey = function () {
+        if (this.me === Person.ALICE) {
+            this.K = this.modExp(this.B, this._a, this._p); //Math.round(Math.pow(this.B, this._a) % this._p);
+        }
+        else {
+            this.K = this.modExp(this.A, this._b, this._p); // Math.round(Math.pow(this.A, this._b) % this._p);
+        }
+    };
+    // From https://stackoverflow.com/a/39870667/811708
+    DiffieHellmanCtrl.prototype.encryptWithXORtoHex = function () {
+        var input = this.message;
+        var c = '';
+        for (var i = 0; i < input.length; i++) {
+            var value1 = input[i].charCodeAt(0);
+            var xorValue = value1 ^ this.K;
+            var xorValueAsHexString = xorValue.toString(16);
+            if (xorValueAsHexString.length < 2) {
+                xorValueAsHexString = '0' + xorValueAsHexString;
+            }
+            c += xorValueAsHexString;
+        }
+        this.encrypted = c;
+    };
+    DiffieHellmanCtrl.prototype.decrypt = function () {
+        var input = this.message;
+        var c = '';
+        for (var i = 0; i < input.length; i += 2) {
+            var value1 = parseInt(input[i].charAt(0) + input[i + 1].charAt(0), 16);
+            var xorValue = value1 ^ this.K;
+            c += String.fromCharCode(xorValue);
+        }
+        this.encrypted = c;
+    };
     return DiffieHellmanCtrl;
 }());
 angular.module('DiffieHellmanApp', []).controller('DiffieHellmanCtrl', DiffieHellmanCtrl);
--- src/diffie-hellman/diffie-hellman.ts
+++ src/diffie-hellman/diffie-hellman.ts
@@ -5,40 +5,30 @@ enum Person {
     BOB = 'Bob'
 }
 
-enum NumberSize {
-    SMALL = 'Small',
-    BIG = 'Big'
-}
-
 class DiffieHellmanCtrl implements ng.IOnInit {
     public me: Person = Person.ALICE;
-    public numberSize: NumberSize = NumberSize.SMALL;
 
-    public _p: bigint;
-    public _g: bigint;
-    public _a: bigint;
-    public _b: bigint;
-    public A: bigint;
-    public B: bigint;
-    public K: bigint;
+    public _p: number;
+    public _g: number;
+    public _a: number;
+    public _b: number;
+    public A: number;
+    public B: number;
+    public K: number;
+
+    public message: string;
+    public encrypted: string;
 
     public $onInit(): void {
-        console.log('Starting up!');
+        console.log('Starting up! I am:', this.me);
     }
 
     // Function for Alice
     public prepareAlice() {
         // Initialize primes based on number size
-        if (this.numberSize === NumberSize.SMALL) {
-            this._p = 23n;
-            this._g = 5n;
-            this._a = 4n;
-        } else {
-            // Group with id 14 from https://tools.ietf.org/html/rfc3526
-            this._p = 32317006071311007300338913926423828248817941241140239112842009751400741706634354222619689417363569347117901737909704191754605873209195028853758986185622153212175412514901774520270235796078236248884246189477587641105928646099411723245426622522193230540919037680524235519125679715870117001058055877651038861847280257976054903569732561526167081339361799541336476559160368317896729073178384589680639671900977202194168647225871031411336429319536193471636533209717077448227988588565369208645296636077250268955505928362751121174096972998068410554359584866583291642136218231078990999448652468262416972035911852507045361090559n;
-            this._g = 2n;
-            this._a = this.getSecretNumber();
-        }
+        this._p = 179;
+        this._g = 2;
+        this._a = this.getSecretNumber();
 
         this.A = this.modExp(this._g, this._a, this._p);
     }
@@ -50,40 +40,20 @@ class DiffieHellmanCtrl implements ng.IOnInit {
     }
 
     // Gets a secret number, for example a or b.
-    private getSecretNumber(): bigint {
-        // https://tools.ietf.org/html/rfc5054#section-3.1: "The private values a and b SHOULD be at least 256-bit
-        // random numbers, to give approximately 128 bits of security against certain methods of calculating discrete
-        // logarithms.
-        // TODO
-        if (this.numberSize === NumberSize.SMALL) {
-            return BigInt(Math.floor(Math.random() * 20));
-        } else {
-            return this.getBigRandomNumber();
-        }
-    }
-
-    private getBigRandomNumber(): bigint {
-        let array = new Uint32Array(8);
-        window.crypto.getRandomValues(array);
-
-        let s = '0x';
-        for (let i = 0; i < array.length; i++) {
-            s += array[i].toString(16);
-        }
-
-        return BigInt(s);
+    private getSecretNumber(): number {
+        return Math.floor(Math.random() * 1000);
     }
 
     // Fast modular exponentiation for a ^ b mod n
     // from https://gist.github.com/krzkaczor/0bdba0ee9555659ae5fe
-    private modExp(a: bigint, b: bigint, n: bigint) {
+    private modExp(a: number, b: number, n: number) {
         a = a % n;
-        let result: bigint = 1n;
+        let result: number = 1;
         let x = a;
         while (b > 0) {
-            let leastSignificantBit: bigint = b % 2n;
-            b = b / 2n;
-            if (leastSignificantBit == 1n) {
+            let leastSignificantBit: number = b % 2;
+            b = b / 2;
+            if (leastSignificantBit === 1) {
                 result = result * x;
                 result = result % n;
             }
@@ -91,7 +61,45 @@ class DiffieHellmanCtrl implements ng.IOnInit {
             x = x % n;
         }
         return result;
-    };
+    }
+
+    public calculateKey(): void {
+        if (this.me === Person.ALICE) {
+            this.K = this.modExp(this.B, this._a, this._p); //Math.round(Math.pow(this.B, this._a) % this._p);
+        } else {
+            this.K = this.modExp(this.A, this._b, this._p); // Math.round(Math.pow(this.A, this._b) % this._p);
+        }
+    }
+
+    // From https://stackoverflow.com/a/39870667/811708
+    private encryptWithXORtoHex() {
+        let input = this.message;
+        let c = '';
+        for (let i = 0; i < input.length; i++) {
+            let value1 = input[i].charCodeAt(0);
+
+            let xorValue = value1 ^ this.K;
+            let xorValueAsHexString = xorValue.toString(16);
+
+            if (xorValueAsHexString.length < 2) {
+                xorValueAsHexString = '0' + xorValueAsHexString;
+            }
+
+            c += xorValueAsHexString;
+        }
+        this.encrypted = c;
+    }
+
+    private decrypt() {
+        let input = this.message;
+        let c = '';
+        for (let i = 0; i < input.length; i += 2) {
+            let value1 = parseInt(input[i].charAt(0) + input[i + 1].charAt(0), 16);
+            let xorValue = value1 ^ this.K;
+            c += String.fromCharCode(xorValue);
+        }
+        this.encrypted = c;
+    }
 }
 
 angular.module('DiffieHellmanApp', []).controller('DiffieHellmanCtrl', DiffieHellmanCtrl);
